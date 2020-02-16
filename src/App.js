050ac049a4f6db2ea6b6a4f4da815b620705c74b
= <=>
2020-02-16 15:15:05 +0700

    project light novel bookstore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import TopMenu from "./components/TopMenu";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <TopMenu />
+          <Switch>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/signin">
+              <SignIn />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/TopMenu.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+  Input
+} from "reactstrap";
+
+const TopMenu = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Light Novel</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/bestsellers">Bestsellers</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Category
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Isekai</DropdownItem>
+                <DropdownItem>Magic</DropdownItem>
+                <DropdownItem>School Life</DropdownItem>
+                <DropdownItem>Harem</DropdownItem>
+                <DropdownItem>Adventure</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>All</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <NavLink href="/signin">Login</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/cart">Cart</NavLink>
+            </NavItem>
+            <Input placeholder="Search" />
+          </Nav>
+          <NavbarText>Best LN</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default TopMenu;
